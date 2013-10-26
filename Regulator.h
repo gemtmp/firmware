7c99592cc9628de26d7f8ff0f7e54bd019228935
gem <gem@sw.ru>
2013-10-26 20:46:03 +0400

    use error to calculate d part

--- Regulator.h
+++ Regulator.h
@@ -9,13 +9,14 @@
 #define REGULATOR_H_
 
 typedef int16_t output_t;
-template <typename InputType = int16_t, output_t Max = 32000, output_t Min = -32000, InputType Large = 128>
+template <typename InputType = int16_t, output_t Max = 40000, output_t Min = -40000, InputType Large = 400>
 class Regul {
 public:
 	typedef InputType input_t;
 
-	Regul(input_t target, uint8_t p = 10, uint8_t i = 0, uint8_t d = 32)
-		: target(target), previos(0), integral(0), p(p), i(i), d(d), output(0) {}
+	Regul(input_t target, uint8_t p = 8, uint8_t i = 0, uint8_t d = 48)
+		: target(target), previos(0), integral(0), dValue(0)
+			, p(p), i(i), d(d), output(0) {}
 	output_t step(input_t current) {
 		input_t err = current - target;
 		if (err > Large)
@@ -24,20 +25,23 @@ public:
 			return output=Min;
 		integral += (i * err)/16;
 		integral = limit<8>(integral);
-		output = err * p + integral + (d*(current - previos))/2; // TODO check overflow
+		dValue = (d*(err - previos))/2;
+		output = err * p + integral + dValue; // TODO check overflow
 		output = limit(output);
-		previos = current;
+		previos = err;
 		return output;
 	}
 	void setTarget(input_t t) {
 		target = t;
 	}
-	void reset() { output = 0; integral = 0; }
+	void reset() { output = 0; integral = 0; previos = 0; }
 	input_t getTarget() const { return target; }
 	output_t getOutput() const { return output; }
 	template <class S>
 	S& log(S& s) const {
-		s << "Target: " << getTarget() << ", Output: " << getOutput() << ", i= " << integral;
+		s << "Target: " << getTarget() << ", Output: " << getOutput()
+				<< ", i= " << integral
+				<< ", d= " << dValue;
 		return s;
 	}
 	template <uint8_t div =1>
@@ -53,6 +57,7 @@ private:
 	input_t target;
 	input_t previos;
 	input_t integral;
+	input_t dValue;
 	uint8_t p;
 	uint8_t i;
 	uint8_t d;
